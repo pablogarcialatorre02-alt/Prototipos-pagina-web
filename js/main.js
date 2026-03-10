8be63fe21047d77ff9001811e680b1f2295cfdbf
Pgarlat <pablogarcialatorre02@gmail.com>
2026-03-10 12:36:51 +0100

    Comit defiitivo 3

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("El archivo JS está conectado correctamente.");
+
+// Esto hará que el botón diga hola al hacer click
+document.querySelector('button').addEventListener('click', () => {
+    alert('¡Gracias por hacer clic! En breve personalizaremos esta acción.');
+});
