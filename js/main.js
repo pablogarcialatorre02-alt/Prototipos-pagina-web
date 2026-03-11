79d8d8ba8a239706a39d0c8b6459f7e339041216
Pgarlat <pablogarcialatorre02@gmail.com>
2026-03-11 11:14:21 +0100

    cambios defunitivos 8

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 console.log("El archivo JS está conectado correctamente.");
 
 // Esto hará que el botón diga hola al hacer click
 document.querySelector('button').addEventListener('click', () => {
     alert('¡Gracias por hacer clic! En breve personalizaremos esta acción.');
 });
-=======
-console.log("El archivo JS está conectado correctamente.");
->>>>>>> Pablo
